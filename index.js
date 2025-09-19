678241ac1cfc8f39c3cc444c52aa997b4a964518
ACPURRINOS <acecilia2271@gmail.com>
2025-09-19 07:16:46 -0300

    Actualizar index.js con rutas faltantes

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const PORT = 3000;
 const CustomerRoutes = require('./routes/CustomerRoutes.js');
 const WebCustomerRoutes = require('./routes/WebCustomerRoutes.js');
 const MenuItemRoutes = require('./routes/MenuItemRoutes.js');
+const DeliveryOrderRoutes = require('./routes/DeliveryOrderRoutes');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
@@ -13,6 +14,8 @@ app.set('views', './views');
 
 app.use('/api/menu', MenuItemRoutes);
 app.use('/api/customer', CustomerRoutes);
+app.use('/api/delivery-orders', DeliveryOrderRoutes);
+
 
 //Rutas Web
 app.use('/', WebCustomerRoutes);
